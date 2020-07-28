693307163b9bae097853f786956b8251407112bd
Carlos <carlosdakk@yahoo.com>
2020-07-27 22:29:34 -0700

    Create TOYProblem.js

---
+++ TOYProblem.js
@@ -0,0 +1,10 @@
+for (let i=1; i <= 100; i++) {
+    if (i % 3 == 0)
+        console.log("Fizz");
+    if (i % 5 == 0)
+        console.log("Buzz");
+    if (i % 15 == 0)
+        console.log("FizzBuzz");
+    else
+        console.log(i);
+}
